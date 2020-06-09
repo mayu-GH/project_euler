bce3a537f5b48818ba9202ae95389c69d7cedfc4
mayu <myaosm@gmail.com>
2020-06-09 12:37:42 +0900

    problem13

--- problem13.ts
+++ problem13.ts
@@ -1,4 +1,5 @@
 //100個の50桁の数字の和の最初の10桁を求める
+//下10桁ではなく上10桁
 import * as util from './util/file';
 import * as path from 'path';
 const filepath = path.join(__dirname, 'problem13.txt');
@@ -11,41 +12,47 @@ util.readFile(filepath).then((contents) => {
 //配列に入れる
 function makeArray(contents: string): number[][] {
   const stringArray: string[] = contents.trim().split(/\r?\n/);
-  let resultArray: number[][] = new Array();
+  let result: number[][] = new Array();
   for (let i = 0; i < stringArray.length; i++) {
     let tmp: number[] = new Array();
-    for (let j = 0; j < 50; j++) {
+    for (let j = 0; j < stringArray[i].length; j++) {
       tmp.push(parseInt(stringArray[i].slice(j, j + 1), 10));
     }
-    resultArray.push(tmp);
+    result.push(tmp);
   }
-  return resultArray;
+  return result;
 }
 
 //加算する
-function sumNumbers(numberArray: number[][]) {
+function sumNumbers(numberArray: number[][]): number[] {
   let sumArray: number[] = new Array();
-  let resultArray: number[] = new Array();
 
-  for (let j = 0; j < 50; j++) {
+  //各桁の合計値
+  for (let j = 0; j < numberArray[0].length; j++) {
     let sum = 0;
-    for (let i = 0; i < 100; i++) {
-      sum = sum + numberArray[i][j];
+    for (let i = 0; i < numberArray.length; i++) {
+      sum += numberArray[i][j];
     }
     sumArray.push(sum);
   }
 
-  const tmpArray = sumArray.reverse();
-  resultArray[0] = 0;
+  sumArray = sumArray.reverse();
+  let result: number[] = new Array();
+  result[0] = 0;
 
-  for (let k = 0; k < 50; k++) {
-    let tmp = resultArray[k] + tmpArray[k];
-    resultArray[k] = tmp % 10;
-    if (k + 1 === 50) {
-      break;
-    }
-    resultArray[k + 1] = Math.floor(tmp / 10);
+  //繰り上がり計算
+  for (let k = 0; k < sumArray.length; k++) {
+    let tmp = result[k] + sumArray[k];
+    result[k] = tmp % 10;
+    result[k + 1] = Math.floor(tmp / 10);
   }
 
-  return resultArray.slice(0, 10).reverse();
+  //上1桁繰り上げ
+  if (!(result[result.length - 1] % 10 === 0)) {
+    let tmp = result[result.length - 1];
+    result[result.length - 1] = tmp % 10;
+    result[result.length] = Math.floor(tmp / 10);
+  }
+
+  return result.reverse().slice(0, 10);
 }
