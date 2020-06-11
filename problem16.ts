593cb504420667527c767576bcbe5d4e0f1efd57
mayu <myaosm@gmail.com>
2020-06-11 10:16:11 +0900

    problem16

--- problem16.ts
+++ problem16.ts
@@ -1,6 +1,4 @@
-//console.log('合計' + getSumOfDigits(2, 15));
-console.log('calcPower' + calcPower(2, 15));
-//console.log('Math.pow' + Math.pow(2, 15));
+console.log(getSumOfDigits(2, 1000));
 
 //各桁の合計
 function getSumOfDigits(value: number, power: number) {
@@ -16,17 +14,19 @@ function getSumOfDigits(value: number, power: number) {
 function calcPower(value: number, power: number): number[] {
   let result: number[] = [1];
   for (let i = 1; i <= power; i++) {
-    const tmpArray: number[] = [0];
+    const tmpArray: number[] = [];
     for (let j = 0; j < result.length; j++) {
       //各桁と掛け算
+      if (isNaN(tmpArray[j])) {
+        tmpArray[j] = 0;
+      }
       let tmp = result[j] * value + tmpArray[j];
-      //掛け算
       if (tmp < 10) {
         tmpArray[j] = tmp;
       } else {
         //繰り上がり
         let k = j;
-        while (tmp > 10) {
+        while (tmp >= 10) {
           tmpArray[k] = tmp % 10;
           if (isNaN(tmpArray[k + 1])) {
             tmpArray[k + 1] = 0;
@@ -39,5 +39,5 @@ function calcPower(value: number, power: number): number[] {
     }
     result = tmpArray;
   }
-  return result;
+  return result.reverse();
 }
