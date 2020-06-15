8d5b5aeac5923397a2939d23bff00f8bb4e82ad0
mayu <myaosm@gmail.com>
2020-06-15 09:15:08 +0900

    problem17 完成

--- problem17.ts
+++ problem17.ts
@@ -12,7 +12,7 @@ const numbersA = [
   'ten',
   'eleven',
   'twelve',
-  'thirten',
+  'thirteen',
   'fourteen',
   'fifteen',
   'sixteen',
@@ -20,27 +20,33 @@ const numbersA = [
   'eighteen',
   'nineteen',
 ];
-const numbersB = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-const numbersC = ['hundred', 'thounsand'];
-
-console.log(countHowManyLetters(1, 101));
+const numbersB = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const numbersC = ['hundred', 'thousand'];
 
 function countHowManyLetters(start: number, end: number) {
   let counter = 0;
-  for (let i = start; i <= end; i++) {
-    if (i < 20) {
-      counter += numbersA[i - 1].length;
-    } else if (20 <= i && i < 100) {
-      counter += returnLengthUnder100(i);
-    } else if (100 <= i) {
-      counter += returnLengthUnder1000(i);
-    } else if (1000 <= i && i < 10000) {
-      counter += returnLengthUnder10000(i);
+  if (1 <= start && end <= 1000) {
+    for (let i = start; i <= end; i++) {
+      if (i < 20) {
+        counter += returnLengthUnder20(i);
+      } else if (20 <= i && i < 100) {
+        counter += returnLengthUnder100(i);
+      } else if (100 <= i && i < 1000) {
+        counter += returnLengthUnder1000(i);
+      } else if (1000 <= i && i < 10000) {
+        counter += returnLength10000(i);
+      }
     }
   }
   return counter;
 }
 
+//1～19
+function returnLengthUnder20(value: number): number {
+  return numbersA[value - 1].length;
+}
+
+//20～99
 function returnLengthUnder100(value: number): number {
   let counter = 0;
   if (value % 10 === 0) {
@@ -51,26 +57,26 @@ function returnLengthUnder100(value: number): number {
   return counter;
 }
 
+//100～999
 function returnLengthUnder1000(value: number): number {
   let counter = 0;
   if (value % 100 === 0) {
     counter = numbersA[value / 100 - 1].concat(numbersC[0]).length;
   } else {
     counter = numbersA[Math.floor(value / 100) - 1].concat(numbersC[0]).length;
-    const tmpValue = parseInt(String(value).slice(1, 3), 10);
-    counter += returnLengthUnder100(tmpValue);
+    const tmp = parseInt(String(value).slice(1, 3), 10);
+    if (tmp < 20) {
+      counter += 'and'.length + returnLengthUnder20(tmp);
+    } else {
+      counter += 'and'.length + returnLengthUnder100(tmp);
+    }
   }
   return counter;
 }
 
-function returnLengthUnder10000(value: number): number {
-  let counter = 0;
-  if (value % 1000 === 0) {
-    counter = numbersA[value / 1000 - 1].concat(numbersC[1]).length;
-  } else {
-    counter = numbersA[Math.floor(value / 1000) - 1].concat(numbersC[1]).length;
-    const tmp = parseInt(String(value).slice(1, 4), 10);
-    counter += returnLengthUnder1000(tmp);
-  }
-  return counter;
+//1000
+function returnLength10000(value: number): number {
+  return numbersA[value / 1000 - 1].concat(numbersC[1]).length;
 }
+
+console.log(countHowManyLetters(1, 1000));
