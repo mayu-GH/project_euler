76dba44506bc1bbd6470d81fcfc9c34e270436b9
mayu <myaosm@gmail.com>
2020-06-19 09:29:55 +0900

    problem18 完成

--- problem18.ts
+++ problem18.ts
@@ -18,50 +18,26 @@ function makeArray(contents: string): number[][] {
   return result;
 }
 
-////最大の累計を探す
-//function findMaximumTotal(triangle: number[][]): number {
-//  let total = triangle[0][0]; //最初の値
-//  let j = 0; //最初の列
-//
-//  for (let i = 1; i < triangle.length; i++) {
-//    //次の行の同じ列と次の列の値を比較
-//    //大きいほうをtotalに加算
-//    if (triangle[i][j] > triangle[i][j + 1]) {
-//      total += triangle[i][j];
-//    } else {
-//      total += triangle[i][j + 1];
-//      j++;
-//    }
-//  }
-//
-//  return total;
-//}
-
 //最大の累計を探す
 function findMaximumTotal(triangle: number[][]): number {
-  const tmpArray: number[][] = triangle;
-
-  //全ルート総当たり
+  const tmpArray: number[][] = [];
+  tmpArray[0] = triangle[0];
   for (let i = 1; i < triangle.length; i++) {
+    tmpArray[i] = [0];
     for (let j = 0; j < triangle[i].length; j++) {
       //1行前かつ同列の値を加算
-      const tmp = triangle[i][j] + triangle[i - 1][j];
-      if (tmpArray[i][j] == null) {
+      const tmp: number = triangle[i][j] + tmpArray[i - 1][j];
+      if (tmpArray[i][j] < tmp) {
         tmpArray[i][j] = tmp;
-      } else {
-        if (tmpArray[i][j] < tmp) {
-          tmpArray[i][j] = tmp;
-        }
       }
-      //1行前かつ同列の値を、同列かつ次の行の値に加算
-      if (j + 1 === tmpArray[i].length) {
+      if (j + 1 === triangle[i].length) {
+        //1行前かつ同列の値を、同列かつ次の行の値に加算
         break;
       } else {
-        tmpArray[i][j + 1] = triangle[i][j + 1] + triangle[i - 1][j];
+        tmpArray[i][j + 1] = triangle[i][j + 1] + tmpArray[i - 1][j];
       }
     }
   }
-
-  //最大の値
+  //最終行の中で最大の値を返す
   return Math.max.apply(null, tmpArray[tmpArray.length - 1]);
 }
